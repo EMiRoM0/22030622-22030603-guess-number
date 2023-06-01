7daf425afab5e9d4c83006fa9f46ff2ba8198b90
Emilio Romo <natsutsu04@gmail.com>
2023-06-01 15:20:13 -0600

    feat:The game with the version 0.9, is ready to use!

--- guessNumber.py
+++ guessNumber.py
@@ -1,1 +1,22 @@
-print("1, 2, 3, 4, 5")
+#gonzalez leos america xochitl utm22030603
+#romo calvillo cristobal emilio utm22030622
+#This version of the game is for one person, with a fixed range of 1 to 100, with only 10 chances.
+#This library guarantees us to get a random number that is in the fixed range.
+from random import randint
+TRIES = 9 #This is the number of chances that do you have.
+secretNumber = randint(1, 100) #This is the range of numbers that we have.
+enteredNumber = int(input("Guess the number 1 to 100: "))
+while secretNumber != enteredNumber: #If you put the wrong number that is missing in the range, you'll lost chances.
+    if TRIES > 0:
+        if secretNumber > enteredNumber: #If our number is to low, this will appear like an advice.
+            print("Very low")
+        else:
+            print("Very high") #Or if our number is too high this will appear to analyze and think in more numbers.
+        print("You have", TRIES, "tries left.") #While you still trying to introduce numbers, there will be a counter of tries that do you have.
+        enteredNumber = int(input("Enter another guess: "))
+        TRIES -= 1
+    else:
+        print("The number was:", secretNumber) #If you fail all the chances, the game will show you the random number.
+        break
+if TRIES >= 0:
+    print("Exactly! You guessed in", 9 - TRIES, "tries.") #If you have entered the correct number, this message will appear and the number of tries.
