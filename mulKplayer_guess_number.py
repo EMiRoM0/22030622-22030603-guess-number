c75471773848141dceedf4077a2aaa0d98d31949
Emilio Romo <natsutsu04@gmail.com>
2023-06-06 19:09:10 -0600

    feat: Last commit of the 2nd game

--- mulKplayer_guess_number.py
+++ mulKplayer_guess_number.py
@@ -1,30 +1,35 @@
 #gonzalez leos america xochitl utm22030603
 #romo calvillo cristobal emilio utm22030622
 
-from getpass import getpass
+from getpass import getpass  #Library to be able to enter a number and not have it visible 
 
-TRIES = 90
+TRIES = 90 #Variable to add points to the game
 
 print("This is a two player game, the first player must enter the range of numbers and the second player must try to guess a random number.")
-
-secretNumber = getpass("Enter the secret number: ")
+print("\n")
+secretNumber = getpass("Enter the secret number: ")    #enter the secret number 
 secretNumber = int(secretNumber)
-enteredNumber = int(input("Guess the number: "))
+print("\n")
+enteredNumber = int(input("Guess the number: ")) #enter a number
 
 while secretNumber != enteredNumber:
     if TRIES > 0:
         if secretNumber > enteredNumber:
-            print("Very low")
+            print("Very low")    #very low 
         else:
-            print("Very high")
+            print("Very high")      # you go very high
         print("You have", TRIES, "points left.")
+        print("\n")
         enteredNumber = int(input("Enter another guess: "))
         TRIES -= 10
     else:
-        print("The number was:", secretNumber)
+        print("\n")
+        print("The number was:", secretNumber)             #reveals the secret number
         break
 
-if secretNumber == enteredNumber:
-    print("Exactly! Just spend", 100 - TRIES, "of your 100 points")
+if secretNumber == enteredNumber:             #end of the game
+    print("\n")
+    print("Exactly! Just spend", 100 - TRIES, "of your 100 points")     #if you got it right 
 else:
-    print("You didn't get it right, try again. ")
+    print("\n")
+    print("You didn't get it right, try again. ")       #if you didn't get it right
